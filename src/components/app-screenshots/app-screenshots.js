c4bb9e435d3c2c853b61768d1294ab0ef93195d1
Wallan Pereira <wallan.santos@mjv.com.br>
2018-07-26 14:03:51 -0300

    screeshot finalizado

---
+++ src/components/app-screenshots/app-screenshots.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 5,
+    loop: true,
+    spaceBetween: 30,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
