9812a29cd10e7124466895945a9de0bc37c339c8
Wallan Pereira <wallan.santos@mjv.com.br>
2018-07-27 10:10:55 -0300

    Ajustar score automatico

--- src/components/app-screenshots/app-screenshots.js
+++ src/components/app-screenshots/app-screenshots.js
@@ -4,6 +4,32 @@ var swiper = new Swiper('.swiper-container', {
     spaceBetween: 30,
     pagination: {
         el: '.swiper-pagination',
-        clickable: true,
-    },
+        clickable: true
+    }
 });
+
+//  SCRIPT ABAIXO ARRUMAR PARA EXIBIR APENAS DOIS EM TELAS MENORES
+
+// $(windows).resize(function () {
+//     if (windows.innerWidth < 768) {
+//         var swiper = new Swiper('.swiper-container', {
+//             slidesPerView: 2,
+//             loop: true,
+//             spaceBetween: 30,
+//             pagination: {
+//                 el: '.swiper-pagination',
+//                 clickable: true
+//             }
+//         });
+//     } else {
+//         var swiper = new Swiper('.swiper-container', {
+//             slidesPerView: 5,
+//             loop: true,
+//             spaceBetween: 30,
+//             pagination: {
+//                 el: '.swiper-pagination',
+//                 clickable: true
+//             }
+//         });
+//     }
+// }).trigger("resize");
