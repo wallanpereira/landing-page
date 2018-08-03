2a1addbf9d00b0bc5a23403ec72d22b2ed2380d6
Wallan Pereira <wallan.santos@mjv.com.br>
2018-08-03 14:27:23 -0300

    Ajustar menu responsivel

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,18 +1,22 @@
-jQuery(function () {
-    jQuery(window).scroll(function () {
-        if (jQuery(this).scrollTop() > 40) {
-            $('.header-component').addClass("header-component-dif");
-        } else {
-            $('.header-component').removeClass("header-component-dif");
-        }
-    });
-});
+// jQuery(function () {
+//     jQuery(window).scroll(function () {
+//         if (jQuery(this).scrollTop() > 40) {
+//             $('.header-component').addClass("header-component-dif");
+//         } else {
+//             $('.header-component').removeClass("header-component-dif");
+//         }
+//     });
+// });
 
-$(window).resize(function () {
-    if (window.innerWidth < 800) {
+// $(window).resize(function () {
+//     if (window.innerWidth < 800) {
 
-        $('.btn-menu').click(function () {
-            $('.box-links').slideToggle();
-        });
-    }
-}).trigger("resize");
+//         $('.btn-menu').click(function () {
+//             $('.box-links').slideToggle();
+//         });
+//     }
+// }).trigger("resize");
+
+$('.btn-menu').click(function () {
+    $('.box-links').slideToggle();
+})
