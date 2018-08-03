4d992dcc4b1b31f93d09fe207c3797ab6f2b1bb9
Wallan Pereira <wallan.santos@mjv.com.br>
2018-08-03 15:30:45 -0300

    Ajustar o scroll do header para deixar branco

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,3 +1,17 @@
+$('.btn-menu, .nav-link,').click(function () {
+    $('.box-links').slideToggle();
+});
+$('.btn-menu, .item, .nav-link').mouseout(function(){
+    $('.box-links').slideToggle();
+})
+
+
+
+
+
+
+
+
 // jQuery(function () {
 //     jQuery(window).scroll(function () {
 //         if (jQuery(this).scrollTop() > 40) {
@@ -8,15 +22,15 @@
 //     });
 // });
 
-// $(window).resize(function () {
-//     if (window.innerWidth < 800) {
+$(window).resize(function () {
+    if (window.innerWidth < 990) {
 
-//         $('.btn-menu').click(function () {
-//             $('.box-links').slideToggle();
-//         });
-//     }
-// }).trigger("resize");
+        // $('.btn-menu, .nav-link').click(function () {
+        //     $('.box-links').slideToggle();
+        // });
 
-$('.btn-menu').click(function () {
-    $('.box-links').slideToggle();
-})
+        $('.header-component').addClass("header-component-dif");
+    } else {
+        $('.header-component').removeClass("header-component-dif");
+    }
+}).trigger("resize");
