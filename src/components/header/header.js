9f794b8f12d543dbcf11847136363d9addcf2482
Wallan Pereira <wallan.santos@mjv.com.br>
2018-08-06 16:40:20 -0300

    Ajustar href do header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,17 +1,30 @@
 // JQuery to header-component
 $(document).ready(function () {
+    var widthMenu = $(window).width();
+
     $(window).scroll(function () {
         var scroll = $(window).scrollTop();
         if (scroll > 40) {
             $('.header-component').addClass("header-component-dif");
-        }
-
-        else {
+        } else {
             $('.header-component').removeClass("header-component-dif");
         }
     });
 
-    $('.btn-menu, .nav-link').click(function () {
-        $('.box-links').slideToggle();
-    });
+    if (widthMenu < 850) {
+        $('.btn-menu, .nav-link').click(function () {
+            $('.box-links').slideToggle();
+        });
+    }
 });
+
+$(window).resize(function () {
+    var winMenu = $(this);
+    var mdMenu = 900;
+
+    if (this.window.width() < mdMenu) {
+        $('.btn-menu, .nav-link').click(function () {
+            $('.box-links').slideToggle();
+        });
+    }
+})
