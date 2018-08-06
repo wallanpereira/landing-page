a66f66e6d673aba921d5f6e4ea23a4aeb36e2b77
Wallan Pereira <wallan.santos@mjv.com.br>
2018-08-06 10:56:31 -0300

    Ajuste no screenshots

--- src/components/app-screenshots/app-screenshots.js
+++ src/components/app-screenshots/app-screenshots.js
@@ -1,41 +1,52 @@
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 5,
-    loop: true,
-    spaceBetween: 30,
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true
+$(document).ready(function() {
+    var tela = $(window).width();
+
+    if(tela < 992) {
+        var swiper = new Swiper('.swiper-container', {
+            slidesPerView: 2,
+            loop: true,
+            spaceBetween: 30,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true
+            }
+        });
+    } else {
+        var swiper = new Swiper('.swiper-container', {
+            slidesPerView: 5,
+            loop: true,
+            spaceBetween: 30,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true
+            }
+        });
     }
 });
 
-//  SCRIPT ABAIXO ARRUMAR PARA EXIBIR APENAS DOIS EM TELAS MENORES
-
-// $(windows).resize(function () {
-//     if (windows.innerWidth < 768) {
-//         var swiper = new Swiper('.swiper-container', {
-//             slidesPerView: 2,
-//             loop: true,
-//             spaceBetween: 30,
-//             pagination: {
-//                 el: '.swiper-pagination',
-//                 clickable: true
-//             }
-//         });
-//     } else {
-//         var swiper = new Swiper('.swiper-container', {
-//             slidesPerView: 5,
-//             loop: true,
-//             spaceBetween: 30,
-//             pagination: {
-//                 el: '.swiper-pagination',
-//                 clickable: true
-//             }
-//         });
-//     }
-// }).trigger("resize");
-
-// $(window).on('resize', function(){
-//     var win = $(this);
-//     if (win.hei)
-
-// })
+$(window).resize(function() {
+    var win = $(this); //this = window
+    var md = 800;
+    
+    if (win.width() < md) {
+       var swiper = new Swiper('.swiper-container', {
+           slidesPerView: 2,
+           loop: true,
+           spaceBetween: 30,
+           pagination: {
+               el: '.swiper-pagination',
+               clickable: true
+           }
+       });
+    } else {
+        var swiper = new Swiper('.swiper-container', {
+            slidesPerView: 5,
+            loop: true,
+            spaceBetween: 30,
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true
+            }
+        });
+    }
+});
