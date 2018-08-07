686b4a37cbba94ca0b4ca913b1585dc8633481e5
Wallan Pereira <wallan.santos@mjv.com.br>
2018-08-07 10:00:51 -0300

    Ajuste js do header

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -36,7 +36,7 @@ $('a[href*="#"]:not([href="#"])').click(function () {
         target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
         if (target.length) {
             $('html, body').animate({
-                scrollTop: target.offset().top - 20
+                scrollTop: target.offset().top - 40
             }, 700);
             return false;
         }
