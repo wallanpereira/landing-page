4dd6e45533568c7a1183d0412dd2e3c941465a7c
Wallan Pereira <wallan.santos@mjv.com.br>
2018-08-07 09:59:10 -0300

    Ajuste no autopplay client

--- src/components/clients/clients.js
+++ src/components/clients/clients.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     var cliswiper = new Swiper('.clients .swiper-testemonial', {
         loop: true,
+        autoplay: 4000,
         reventClicks: false,
         preventClicksPropagation: false,
         nextButton: '.swiper-button-next',
